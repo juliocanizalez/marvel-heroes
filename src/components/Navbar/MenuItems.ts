ef59d631e490c5762dddd58341ed07b96c765291
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-18 23:32:29 -0600

    :hammer: Add menu items for navbar

---
+++ src/components/Navbar/MenuItems.ts
@@ -0,0 +1,24 @@
+export enum Home {
+  URL = '/home',
+  TITLE = 'Home',
+}
+
+export enum Characters {
+  URL = '/characters',
+  TITLE = 'Characters',
+}
+
+export enum Bookmarks {
+  URL = '/bookmarks',
+  TITLE = 'Bookmarks',
+}
+
+export enum Stories {
+  URL = '/stories',
+  TITLE = 'Stories',
+}
+
+export enum Comics {
+  URL = '/comics',
+  TITLE = 'Comics',
+}
