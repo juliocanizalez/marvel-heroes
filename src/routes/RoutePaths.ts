953d3f0eacadd712767d5e8f955becebd42da8b0
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-18 18:10:12 -0600

    :wrench: Add route paths for pages

---
+++ src/routes/RoutePaths.ts
@@ -0,0 +1,14 @@
+enum RoutePaths {
+  HOME = '/home',
+  CHARACTER_LIST = '/characters',
+  CHARACTER_DETAILS = '/characters/:idCharacter',
+  CHARACTER_BOOKMARKS = '/bookmarks/characters',
+  COMIC_LIST = '/comics',
+  COMIC_DETAILS = '/comics/:idComic',
+  COMIC_BOOKMARKS = '/bookmarks/comics',
+  STORIES_LIST = '/stories',
+  STORIES_DETAILS = '/stories/:idStory',
+  STORIES_BOOKMARKS = '/bookmarks/stories',
+}
+
+export default RoutePaths;
