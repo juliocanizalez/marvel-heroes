c03f3f3f009169019c5d155500af23e3d081ba28
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-23 00:49:16 -0600

    :hammer: Add utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+/* eslint no-restricted-syntax: 0 */
+export const NavBarLogo =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png';
+
+export const getQueryVariable = (variable: string) => {
+  const query = window.location.search.substring(1);
+  const vars = query.split('&');
+
+  for (const v of vars) {
+    const pair = v.split('=');
+    if (pair[0] === variable) {
+      return pair[1];
+    }
+  }
+
+  return '';
+};
+
+export const shortenText = (text: string, length: number) => {
+  if (text.length <= length) {
+    return text;
+  }
+  let sub = text.substr(0, length);
+  sub += '...';
+  return sub;
+};
