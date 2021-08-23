bbf068dc6037707a873b1547d92a65bece3e3b57
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-22 21:44:46 -0600

    :sparkles: Add loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading: React.FC = () => {
+  return (
+    <div className='loading'>
+      <div className='lds-dual-ring' />
+    </div>
+  );
+};
+
+export default Loading;
