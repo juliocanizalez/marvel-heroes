1ba63d2037801ddb9c0385f611cedc12f7feb137
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-23 15:09:44 -0600

    :sparkles: Add not found component

---
+++ src/containers/NotFound/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import './NotFound.scss';
+
+const NotFound: React.FC = () => (
+  <div className='not-found'>
+    <Player
+      autoplay
+      loop
+      style={{ width: '400px', height: '400px' }}
+      src='https://assets10.lottiefiles.com/packages/lf20_ghfpce1h.json'
+    >
+      <Controls visible={false} />
+    </Player>
+  </div>
+);
+
+export default NotFound;
