b1f2f1688f491753835000b0b619cd8d2d311874
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-23 15:28:38 -0600

    :hammer: Add more routes

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -12,21 +12,27 @@ const Home = lazy(() => import('../containers/Home/Home'));
 const Characters = lazy(() => import('../containers/Characters/Characters'));
 const Stories = lazy(() => import('../containers/Stories/Stories'));
 const Comics = lazy(() => import('../containers/Comics/Comics'));
+const CharactersBookmarks = lazy(() => import('../containers/Bookmarks/CharacterBookmarks'));
+const StoriesBookmarks = lazy(() => import('../containers/Bookmarks/StoriesBookmarks'));
+const ComicsBookmarks = lazy(() => import('../containers/Bookmarks/ComicBookmarks'));
 
 const App: React.FC = () => (
   <Provider store={store}>
     <PersistGate loading={<Loading />} persistor={persistor}>
       <Router>
+        <Navbar />
         <Suspense fallback={<Loading />}>
           <Route exact path='/'>
             <Redirect to={RoutePaths.HOME} />
           </Route>
-          <Navbar />
           <Switch>
             <Route exact path={RoutePaths.HOME} component={Home} />
             <Route exact path={RoutePaths.COMIC_LIST} component={Comics} />
             <Route exact path={RoutePaths.CHARACTER_LIST} component={Characters} />
             <Route exact path={RoutePaths.STORIES_LIST} component={Stories} />
+            <Route exact path={RoutePaths.CHARACTER_BOOKMARKS} component={CharactersBookmarks} />
+            <Route exact path={RoutePaths.STORIES_BOOKMARKS} component={StoriesBookmarks} />
+            <Route exact path={RoutePaths.COMIC_BOOKMARKS} component={ComicsBookmarks} />
           </Switch>
         </Suspense>
       </Router>
