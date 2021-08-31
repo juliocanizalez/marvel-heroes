1c2758b80b23108e7ee5504398ace223ac8f6ed5
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-30 20:25:56 -0600

    :hammer: Update image place holder

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,8 @@
 export const NavBarLogo =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png';
 
-export const imagePlaceHolder = 'https://coacademy-server-jc.com/uploads/courses/images/890.jpg';
+export const imagePlaceHolder =
+  'https://www.morrispublishing.com/images/comic-book-publishing/features-options/ComicSpread.jpg';
 
 export const getQueryVariable = (variable: string) => {
   const query = window.location.search.substring(1);
